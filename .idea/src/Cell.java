7369960ecfb58fe7ac068b72debcb6ebf3db77cb
Akshay Khale <akshaykhale22@gmail.com>
2023-08-02 16:42:50 -0400

    stop work for now

--- .idea/src/Cell.java
+++ .idea/src/Cell.java
@@ -3,7 +3,11 @@ public class Cell {
 
     public Cell(int strength, int x, int y, int id){
         this.strength = 0;
-
+        this.x = 0;
+        this.y = 0;
+        this.id = 0;
     }
-
+    public Cell(){
+        this(0,0,3,1);
+    }
 }
