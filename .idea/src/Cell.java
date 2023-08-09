e4ee6af3a577c950937af80fec7cc8ab3162ff6f
Akshay Khale <akshaykhale22@gmail.com>
2023-08-09 17:00:45 -0400

    assignment 3

--- .idea/src/Cell.java
+++ .idea/src/Cell.java
@@ -2,15 +2,16 @@ import java.util.ArrayList;
 
 public class Cell {
     public int strength, x, y, id; //get and set
+    public static void main(String[] args) {
+        Cell defaultCell = new Cell(0,0,0,0);
 
+
+    }
     public Cell(int strength, int x, int y, int id){
         this.strength = strength;
         this.x = x;
         this.y = y;
         this.id = id;
-    }
-    public Cell(){
-        this(0,0,0,0);
     }
 
     public void interactNeighbours(ArrayList<Cell> neighbours){
--- .idea/src/main.java
+++ .idea/src/main.java
@@ -1,34 +1,4 @@
 
 class main {
-    public static void main(String[] args) {
-        int[] boo = {0, 1, 2, 3, 4};
 
-        int[] THISVARIABLEHASBEENCHANGED = new int[100];
-        for(int i = 0; i < 100; i++){
-            THISVARIABLEHASBEENCHANGED[i] = i;
-        }
-        System.out.println("The number of even numbers in boo is:");
-        System.out.println(countEvenNum(boo, 5));
-
-        System.out.println("The number of even numbers in THISVARIABLEHASBEENCHANGED is:");
-        System.out.println(countEvenNum(THISVARIABLEHASBEENCHANGED, 100));
-
-    }
-
-
-
-    private static int countEvenNum(int[] arr, int size){
-        int count = 0;
-
-        for(int i = 0; i < size; i++){
-            if(arr[i] % 2 == 0){
-                count++;
-            }
-            else{
-
-            }
-        }
-
-        return count;
-    }
 }
