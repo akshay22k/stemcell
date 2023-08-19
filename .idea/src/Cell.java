9e30d52fd176fe8904938384781c2d1a1db5828a
Akshay Khale <akshaykhale22@gmail.com>
2023-08-19 09:37:04 -0400

    assignment 3

--- .idea/src/Cell.java
+++ .idea/src/Cell.java
@@ -1,20 +1,56 @@
 import java.util.ArrayList;
 
 public class Cell {
-    public int strength, x, y, id; //get and set
-    public static void main(String[] args) {
-        Cell defaultCell = new Cell(0,0,0,0);
+    public int strength, x, y, id;
 
-
-    }
     public Cell(int strength, int x, int y, int id){
-        this.strength = strength;
-        this.x = x;
-        this.y = y;
-        this.id = id;
+        setStrength(strength);
+        setX(x);
+        setY(y);
+        setId(id);
     }
 
+    //getters and setters follow
+    public void setStrength(int strength){
+        if(strength > 0){
+            this.strength =  strength;
+        }
+        else{
+            strength = 0;
+        }
+    }
+    public void setX(int x){
+        if(x > 0){
+            this.x =  x;
+        }
+        else{
+            x = 0;
+        }
+    }
+    public void setY(int y){
+        if(y > 0){
+            this.y =  y;
+        }
+        else{
+            y = 0;
+        }
+    }
+    public void setId(int id){
+        if(id > 0){
+            this.id =  id;
+        }
+        else{
+            id = 0;
+        }
+    }
+    //save for later
     public void interactNeighbours(ArrayList<Cell> neighbours){
 
     }
+
+    public static void main(String[] args) {
+        Cell defaultCell = new Cell(0,0,0,0);
+    }
+
+
 }
